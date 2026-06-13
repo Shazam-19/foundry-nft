2a88987abea085f7d422dbec4acaa910ea5b9577
shazam <abdelrahman.sayed2015.p6636@gmail.com>
2026-06-13 21:52:14 +0300

    Add mint & tokenURI funcs and NFT img

--- src/BasicNFT.sol
+++ src/BasicNFT.sol
@@ -4,7 +4,20 @@ pragma solidity ^0.8.18;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract BasicNFT is ERC721 {
-    uint256 private _tokenIdCounter;
+    uint256 private s_tokenIdCounter;
 
-    constructor() ERC721("Dogie", "DOG") {}
+    constructor() ERC721("Dogie", "DOG") {
+        s_tokenIdCounter = 0;
+    }
+
+    function mintNft() public returns (uint256) {
+        uint256 newTokenId = s_tokenIdCounter;
+        _safeMint(msg.sender, newTokenId);
+        s_tokenIdCounter += 1;
+        return newTokenId;
+    }
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        return "ipfs://bafybeigs5lojzzear4gyuizxgmdabdam7jftguarspv4rcxw665pkioxdy/3054";
+    }
 }
